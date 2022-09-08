e4b3acd8aedffd085b4ac2267891f24001699861
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-08 09:01:30 -0300

    Testes Cliente, Calculador e Objetos para teste

---
+++ src/test/java/br/com/alura/livraria/dominio/ClienteTest.java
@@ -0,0 +1,21 @@
+package br.com.alura.livraria.dominio;
+
+import org.junit.jupiter.api.Test;
+
+import static br.com.alura.livraria.dominio.ObjetosParaTestes.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ClienteTest {
+    @Test
+    void igualdade() {
+        Cliente outroCliente = new Cliente(ID_CLIENTE, UM_TELEFONE);
+
+        assertEquals(UM_CLIENTE, outroCliente);
+        assertEquals(UM_CLIENTE.hashCode(), outroCliente.hashCode());
+    }
+
+    @Test
+     void lerTelefoneFormatado() {
+        assertEquals(UM_CLIENTE.getTelefone(), "(11) 55555555");
+    }
+}
