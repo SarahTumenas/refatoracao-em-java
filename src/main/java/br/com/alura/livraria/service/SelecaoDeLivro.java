2f966b8125f799dca5affeee89b9f1176761d1df
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-09 15:52:01 -0300

    Extrais classe

--- src/main/java/br/com/alura/livraria/service/SelecaoDeLivro.java
+++ src/main/java/br/com/alura/livraria/service/SelecaoDeLivro.java
@@ -3,36 +3,25 @@ package br.com.alura.livraria.service;
 import br.com.alura.livraria.dominio.*;
 import org.javamoney.moneta.Money;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static java.time.LocalDate.now;
 
 public class SelecaoDeLivro {
 
     private final TodosLivros todosLivros;
     private final CalculadoraFrete calculadoraFrete;
-    private final Set<CarrinhoDeCompras> carrinhos;
+    private final CarrinhoDeComprasFactory carrinhoFactory;
 
 
     public SelecaoDeLivro(TodosLivros todosLivros, CalculadoraFrete calculadoraFrete) {
         this.todosLivros = todosLivros;
         this.calculadoraFrete = calculadoraFrete;
-        this.carrinhos = new HashSet<>();
+        this.carrinhoFactory = new CarrinhoDeComprasFactory();
     }
 
     public CarrinhoDeCompras adicionarLivroNoCarrinhoDoCliente(ISBN isbn, Cliente cliente) {
         Livro livro = todosLivros.por(isbn);
         Money valorFrete = calculadoraFrete.baseadoEm(cliente.getCep());
-
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras(cliente, livro, valorFrete, now());
-        if(carrinhos.contains(carrinho)) {
-            carrinho = carrinhos.stream()
-                    .filter(umCarrinho -> umCarrinho.doCliente(cliente))
-                    .findFirst().orElse(null);
-            if(carrinho != null)carrinho.getLivros().adicionar(livro);
-        }
-        else carrinhos.add(carrinho);
+        CarrinhoDeCompras carrinho = carrinhoFactory.obterCarrinho(cliente, livro, valorFrete);
         return carrinho;
     }
-}
+
+    }
